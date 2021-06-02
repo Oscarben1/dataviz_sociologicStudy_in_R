ce47b82a67052242e7e2348cff961df457ef8039
Oscarben1 <71690956+Oscarben1@users.noreply.github.com>
2021-06-02 16:09:07 +0200

    shiny app oscar côté notes

--- shiny/server.R
+++ shiny/server.R
@@ -4,16 +4,41 @@ library(dplyr)
 library(plotly)
 library(readr)
 
-dataset <- read_csv("../data/student-por.csv") %>% filter(sex==input$sex)
+dataset <- read_csv("../data/student-por.csv")
+dataset2 <- read_csv("../data/student-por.csv")
+dataset3 <- read_csv("../data/student-por.csv")
 
 server <- function(input, output){
-    
+
     vals <- reactiveValues(keeprows = rep(TRUE, nrow(dataset)),
                            dataset = dataset)
-    
-    output$meanStudytimeM <- renderValueBox({
-        valueBox(mean(vals$dataset$studytime), "Temps moyen de travail garçon", icon = icon("list"), color = "red")
+
+
+
+    output$nbIndMean <- renderValueBox({
+        valueBox(dim(vals$dataset)[1], "nb d'élèves au dessus de la mean", icon = icon("book"), color = "red")
     })
-    
-    
+
+    output$studytime <- renderValueBox({
+        valueBox(signif(mean(vals$dataset$studytime), 4), "Temps de travail", icon = icon("clock"), color = "blue")
+    })
+
+    output$failures <- renderValueBox({
+        valueBox(signif(mean(vals$dataset2$failures), 4), "Nb d'échecs en dessous de la mean", icon = icon("sad-tear"), color = "green")
+    })
+
+    observeEvent(input$individusMean,{
+        vals$dataset <- read_csv("../data/student-por.csv") %>% filter(G1 >= input$individusMean)
+        vals$dataset2 <- read_csv("../data/student-por.csv") %>% filter(G1 <= input$individusMean)
+    })
+
+    observeEvent(input$travail_père,{
+        vals$dataset3 <- read_csv("../data/student-por.csv") %>% filter(Fjob == input$travail_père)
+    })
+
+    output$G1andMean <- renderPlotly({
+        temp_plot <- ggplot(vals$dataset3 %>% filter(age <= 19) %>% group_by(age) %>% mutate(mean_G1 = mean(G1)) %>% distinct(age, mean_G1), aes(x = age, y = mean_G1)) + geom_bar(stat = "identity")
+        ggplotly(temp_plot)
+    })
+
 }
--- shiny/ui.R
+++ shiny/ui.R
@@ -13,47 +13,57 @@ dashboardPage(
             menuItem("Relations familiales", tabName = "relations_familiales", icon=icon("star"))
         ),
 
-        sliderInput("moyenne_générale" ,"Moyenne générale", min = 0, max = 20, value = c(5, 16)),
-        
-        radioButtons("sex", label = h3("Sexe"), 
+        sliderInput("individusMean" ,"Moyenne de l'élève", 4, 19, 10),
+
+
+        radioButtons("meanStudyTime", label = h3("Sexe"),
                      choices = list("Garçon" = "M", "Fille" = "F"),
                      selected = "M"),
-        
-        radioButtons("adresse", label = h3("Lieu de vie"), 
+
+        radioButtons("adresse", label = h3("Lieu de vie"),
                      choices = list("Ville" = 1, "Campagne" = 2, "Les 2" = 3),
                      selected = 3),
-        
-        checkboxGroupInput("age", label = h3("Age"), 
+
+        checkboxGroupInput("age", label = h3("Age"),
                      choices = list("15" = 1, "16" = 2, "17" = 3, "18" = 4, "19" = 5, "20" = 6, "21" = 7, "22" = 8, "Tous" = 9),
                      selected = 9),
-        
-        selectInput("travail_mère", label = h3("Travail de la mère"), 
+
+        selectInput("travail_mère", label = h3("Travail de la mère"),
                            choices = list("Education" = 1, "Santé" = 2, "Service publique" = 3, "Au foyer" = 4, "Autre" = 5, "Tous" = 6),
                     selected = 6),
-        
-        selectInput("travail_père", label = h3("Travail du père"), 
-                    choices = list("Education" = 1, "Santé" = 2, "Service publique" = 3, "Au foyer" = 4, "Autre" = 5, "Tous" = 6),
-                    selected = 6),
-        
+
+        selectInput("travail_père", label = h3("Travail du père"),
+                    choices = list("Education" = "teacher", "Santé" = "health", "Service publique" = "services", "Au foyer" = "at_home", "Autre" = "other"),
+                    selected = "teacher"),
+
         sliderInput("temps_de_travail" ,"Temps de travail", min = 1, max = 4, value = c(1, 4)),
-        
+
         sliderInput("consommation_alcool_hebdo" ,"Consommation d'alcool hebdo", min = 1, max = 5, value = c(1, 5))
-        
+
     ),
- 
+
     dashboardBody(
         tabItems(
             tabItem(
                 tabName = "notes",
                 fluidRow(
-                    valueBoxOutput("meanStudytimeM"),
+                    valueBoxOutput("nbIndMean"),
+                    valueBoxOutput("studytime"),
+                    valueBoxOutput("failures"),
                 ),
+                fluidRow(
+                    box(
+                        title = "Notes en fonction de l'âge, et des parents",
+                        status = "primary",
+                        solidHeader = TRUE,
+                        plotlyOutput("G1andMean")
+                ))
             ),
             tabItem(tabName = "santé",
-                
+
             ),
             tabItem(tabName = "relations_familiales",
-                    
+
             )
         ),
     ),
